0109a240e84a8ef53e93c4305beaafac26bda18d
Lokesh Devnani <lokeshthegenius@gmail.com>
2016-12-15 02:28:33 +0530

    Oninit implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -8,14 +8,20 @@ import { HeroService } from './hero.service';
   styleUrls: ['./app.component.css'],
   providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes:Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   constructor(private heroService: HeroService){
+    //
+  }
+  getHeroes(): void {
     this.heroes = this.heroService.getHeroes();
   }
 }
