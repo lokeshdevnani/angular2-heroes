1b44011cda071ac676caff926b6f31856558843b
Lokesh Devnani <lokeshthegenius@gmail.com>
2016-12-15 13:10:07 +0530

    basic routing added and routerlinks displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a routerLink="/dashboard">Dashboard</a>
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
